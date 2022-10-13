c8804dae5a9017fad2cb68341e81b287fbaaeeff
Anton <anty-sem2010@mail.ru>
2022-10-13 19:03:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log('arr' + "- object");
+console.log('4' + "- object");
+
+let incr = 10, 
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+    console.log(incr++);
+    console.log(decr-- );
